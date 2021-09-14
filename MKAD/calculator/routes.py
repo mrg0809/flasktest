f2f66097bd06b4f39d70859170984e288a2a7fac
Marcelino Rodriguez <mrg0809@gmail.com>
2021-09-13 23:13:52 -0500

    se agrega index con form y conversion de direccion a coordenadas

--- MKAD/calculator/routes.py
+++ MKAD/calculator/routes.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from decimal import Decimal
+from yandex_geocoder import Client
 
 calculator = Blueprint('calculator', __name__)
 
+client = Client("0dc2619f-d090-4dcc-85da-0446f3e3ccfd")
+
+'''address = client.coordinates(c1)'''
+
 @calculator.route('/')
 def index():
+    return render_template('index.html')
+
+@calculator.route('/distance/<address>', methods=['GET'])
+def distance(address):
+    print(address)
     return '<h1>Welcome</h1>'
